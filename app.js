532260fac528759fe3c63bcee72b452f22468c21
Deepak Bansal <deepakbansal@Deepaks-MacBook-Pro.local>
2020-11-24 20:58:05 +0530

    change in app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ require('express-async-errors');
 const cookieSession=require('cookie-session');
 const app=express();
 app.use(express.json());
-app.use(cookieSession({
-    signed:false
-}))
+// app.use(cookieSession({
+//     signed:false
+// }))
 const {router:allRoutes}=require('./routes/allRoutes');
 const {errorHandler}=require('./middleware/errorHandler');
 app.use(allRoutes);
